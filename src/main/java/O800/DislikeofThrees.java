a5b998259205b15fd5213a83b7d0943eaa6954f2
Anubhav Pandey <57632430+SpeedyAstro@users.noreply.github.com>
2024-07-04 13:53:20 +0530

    800

--- src/main/java/O800/DislikeofThrees.java
+++ src/main/java/O800/DislikeofThrees.java
@@ -8,17 +8,13 @@ public class DislikeofThrees {
         int t = sc.nextInt();
         while (t-->0){
             int k = sc.nextInt();
-            System.out.println(solve(k-1)==1?1:solve(k-1)+1);
+            for (int i = 1 ;;i++){
+                if (i%3==0 || i%10==3) continue;
+                if (--k==0){
+                    System.out.println(i);
+                    break;
+                }
+            }
         }
-    }
-    public static int solve(int k){
-        int num3 = 0;
-        if (k<=10) {
-            if (k>=3) num3 = 1;
-            else num3 = 0;
-        }
-        else num3 = k/10;
-        int div3 = k/3>=2?(k/3)-1:0;
-        return (k+num3+div3)%3==0?(k+num3+div3+1):(k+num3+div3);
     }
 }
